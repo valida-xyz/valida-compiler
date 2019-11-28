9eb8b55f18344018ec965271c02b1404eac70974
Dominik Montada <dominik.montada@hightec-rt.com>
2019-11-28 15:35:18 +0100

    [DPWBS-1138] feat(InstructionSelector): select G_INTTOPTR and G_PTRTOINT to COPY
    G_INTTOPTR and G_PTRTOINT translate to a simple COPY. Therefore it
    is enough to constrain the destination register and change the
    op-code to COPY.
    
    add G_INTTOPTR and G_PTRTOINT selection tests

--- llvm/lib/Target/TriCore/TriCoreInstructionSelector.cpp
+++ llvm/lib/Target/TriCore/TriCoreInstructionSelector.cpp
@@ -60,12 +60,12 @@ private:
                           const Register &DestReg,
                           MachineRegisterInfo &MRI) const;
 
-  bool constrainDstRegister(MachineInstr &I, MachineRegisterInfo &MRI) const;
   bool selectBrCond(MachineInstr &I, const MachineRegisterInfo &MRI) const;
   bool selectBrIndirect(MachineInstr &I, const MachineRegisterInfo &MRI) const;
   bool selectConstant(MachineInstr &I, MachineRegisterInfo &MRI) const;
   bool selectCmpAndJump(MachineInstr &I, const MachineRegisterInfo &MRI,
                         MachineIRBuilder &MIRBuilder) const;
+  bool selectCopy(MachineInstr &I, MachineRegisterInfo &MRI) const;
   bool selectGlobalValue(MachineInstr &I, MachineRegisterInfo &MRI) const;
   bool selectICmp(MachineInstr &I, const MachineRegisterInfo &MRI) const;
   bool selectLoadStore(MachineInstr &I, const MachineRegisterInfo &MRI) const;
@@ -316,7 +316,7 @@ bool TriCoreInstructionSelector::select(MachineInstr &I) {
 
     // Constrain destination register for COPYs and PHIs
     if (I.isCopy() || I.isPHI())
-      return constrainDstRegister(I, MRI);
+      return selectCopy(I, MRI);
 
     return true;
   }
@@ -343,6 +343,9 @@ bool TriCoreInstructionSelector::select(MachineInstr &I) {
     return selectGlobalValue(I, MRI);
   case TargetOpcode::G_ICMP:
     return selectICmp(I, MRI);
+  case TargetOpcode::G_INTTOPTR:
+  case TargetOpcode::G_PTRTOINT:
+    return selectCopy(I, MRI);
   case TargetOpcode::G_LOAD:
   case TargetOpcode::G_STORE:
     return selectLoadStore(I, MRI);
@@ -403,75 +406,6 @@ void TriCoreInstructionSelector::materializePointer(
 
   constrainSelectedInstRegOperands(*MovMI, TII, TRI, RBI);
   constrainSelectedInstRegOperands(*LeaMI, TII, TRI, RBI);
-}
-
-bool TriCoreInstructionSelector::constrainDstRegister(
-    MachineInstr &I, MachineRegisterInfo &MRI) const {
-  // Constrain the destination register for COPY, G_PHI and PHI
-
-  // COPY requires SrcReg and DstReg to have matching types:
-  //    if the type is still available:
-  //      types must match
-  //    if one of the operands has a register class set:
-  //      other operand must have the same size as the register class
-  //
-  // Therefore we can only have the following COPYs:
-  //    %1:*regbank(s*) = COPY %0:*regbank(s*)
-  //
-  //    %1:*regbank(s32) = COPY %0:{addr,data}regs
-  //
-  //    %1:{addr,data}regs = COPY %0:*regbank(s32)
-  //
-  //    %1:{addr,data}regs = COPY %0:{addr,data}regs
-  //
-  //    %1:ext{addr,data}regs = COPY %0:ext{addr,data}regs
-  //
-  // The following COPYs are not possible (given different register banks for
-  // the 2nd example)
-  //    %1:ext{addr,data}regs = COPY %0:*regbank(s32)
-  // Because s32 has 32-bits but the destination is 64 bits and
-  //
-  //    %1:*regbank(s32) = COPY %0:ext{addr,data}regs.SubRegIdx
-  // Because we are the only one who can emit a subregister copy and in that
-  // case we make sure that the register banks are the same
-  //
-  // The same holds true for G_PHI/PHI
-  //
-  // Therefore we can be sure that SrcSize and DstSize are the same
-
-  const Register DstReg = I.getOperand(0).getReg();
-  const Register SrcReg = I.getOperand(1).getReg();
-  unsigned DstSize = RBI.getSizeInBits(DstReg, MRI, TRI);
-  unsigned SrcSize = RBI.getSizeInBits(SrcReg, MRI, TRI);
-
-  assert(DstSize == SrcSize && "Unexpected illegal subregister copy");
-
-  // Find the correct register classes for the source and destination registers.
-  const RegisterBank &DstRegBank = *RBI.getRegBank(DstReg, MRI, TRI);
-  const TargetRegisterClass *DstRC = getRegClassForRegBank(DstRegBank, DstSize);
-
-  if (!DstRC) {
-    LLVM_DEBUG(dbgs() << "Unexpected register size " << DstSize << '\n');
-    return false;
-  }
-
-  // In case of G_PHI, we need to set the OpCode to PHI
-  if (I.isPHI())
-    I.setDesc(TII.get(TargetOpcode::PHI));
-
-  // Nothing to do if DstReg is a physical register
-  if (Register::isPhysicalRegister(DstReg))
-    return true;
-
-  // And constrain the destination. No need to constrain the source register
-  // as it will be constrained once we reach another of its uses or defs.
-  if (!TriCoreRegisterBankInfo::constrainGenericRegister(DstReg, *DstRC, MRI)) {
-    LLVM_DEBUG(
-        dbgs() << "Failed to constrain COPY/G_PHI/PHI destination operand\n");
-    return false;
-  }
-
-  return true;
 }
 
 bool TriCoreInstructionSelector::selectBrCond(
@@ -639,6 +573,82 @@ bool TriCoreInstructionSelector::selectCmpAndJump(
   constrainSelectedInstRegOperands(*JumpMI, TII, TRI, RBI);
 
   I.removeFromParent();
+  return true;
+}
+
+bool TriCoreInstructionSelector::selectCopy(MachineInstr &I,
+                                            MachineRegisterInfo &MRI) const {
+  const unsigned Opc = I.getOpcode();
+  assert((Opc == TargetOpcode::COPY || Opc == TargetOpcode::PHI ||
+          Opc == TargetOpcode::G_PHI || Opc == TargetOpcode::G_INTTOPTR ||
+          Opc == TargetOpcode::G_PTRTOINT) &&
+         "Unexpected instruction");
+
+  // Constrain the destination register for COPY, G_PHI and PHI
+
+  // COPY requires SrcReg and DstReg to have matching types:
+  //    if the type is still available:
+  //      types must match
+  //    if one of the operands has a register class set:
+  //      other operand must have the same size as the register class
+  //
+  // Therefore we can only have the following COPYs:
+  //    %1:*regbank(s*) = COPY %0:*regbank(s*)
+  //
+  //    %1:*regbank(s32) = COPY %0:{addr,data}regs
+  //
+  //    %1:{addr,data}regs = COPY %0:*regbank(s32)
+  //
+  //    %1:{addr,data}regs = COPY %0:{addr,data}regs
+  //
+  //    %1:ext{addr,data}regs = COPY %0:ext{addr,data}regs
+  //
+  // The following COPYs are not possible (given different register banks for
+  // the 2nd example)
+  //    %1:ext{addr,data}regs = COPY %0:*regbank(s32)
+  // Because s32 has 32-bits but the destination is 64 bits and
+  //
+  //    %1:*regbank(s32) = COPY %0:ext{addr,data}regs.SubRegIdx
+  // Because we are the only one who can emit a subregister copy and in that
+  // case we make sure that the register banks are the same
+  //
+  // All other instructions that are selected to a COPY/PHI are also legalized
+  // to have the same type size.
+  //
+  // Therefore we can be sure that SrcSize and DstSize are the same
+
+  const Register DstReg = I.getOperand(0).getReg();
+  const Register SrcReg = I.getOperand(1).getReg();
+  unsigned DstSize = RBI.getSizeInBits(DstReg, MRI, TRI);
+  unsigned SrcSize = RBI.getSizeInBits(SrcReg, MRI, TRI);
+
+  assert(DstSize == SrcSize && "Unexpected illegal subregister copy");
+
+  // Find the correct register classes for the source and destination registers.
+  const RegisterBank &DstRegBank = *RBI.getRegBank(DstReg, MRI, TRI);
+  const TargetRegisterClass *DstRC = getRegClassForRegBank(DstRegBank, DstSize);
+
+  if (!DstRC) {
+    LLVM_DEBUG(dbgs() << "Unexpected register size " << DstSize << '\n');
+    return false;
+  }
+
+  // Set the expected OpCode: PHI for G_PHI, COPY for anything else
+  const unsigned OpCode = I.isPHI() ? TargetOpcode::PHI : TargetOpcode::COPY;
+  I.setDesc(TII.get(OpCode));
+
+  // Nothing to do if DstReg is a physical register
+  if (Register::isPhysicalRegister(DstReg))
+    return true;
+
+  // And constrain the destination. No need to constrain the source register
+  // as it will be constrained once we reach another of its uses or defs.
+  if (!TriCoreRegisterBankInfo::constrainGenericRegister(DstReg, *DstRC, MRI)) {
+    LLVM_DEBUG(
+        dbgs() << "Failed to constrain COPY/G_PHI/PHI destination operand\n");
+    return false;
+  }
+
   return true;
 }
 
