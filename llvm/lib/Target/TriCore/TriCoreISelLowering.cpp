dece82396c6d74f66770332567e69333747259ed
Dominik Montada <dominik.montada@hightec-rt.com>
2020-02-06 12:44:42 +0100

    [DPWBS-1210] chore(TargetLowering): remove unused includes

--- llvm/lib/Target/TriCore/TriCoreISelLowering.cpp
+++ llvm/lib/Target/TriCore/TriCoreISelLowering.cpp
@@ -16,21 +16,11 @@
 #include "TriCoreRegisterInfo.h"
 #include "TriCoreSubtarget.h"
 #include "TriCoreTargetMachine.h"
-#include "llvm/ADT/SmallSet.h"
-#include "llvm/ADT/Statistic.h"
-#include "llvm/CodeGen/CallingConvLower.h"
-#include "llvm/CodeGen/MachineFrameInfo.h"
-#include "llvm/CodeGen/MachineFunction.h"
-#include "llvm/CodeGen/MachineInstrBuilder.h"
-#include "llvm/CodeGen/MachineRegisterInfo.h"
 #include "llvm/CodeGen/SelectionDAGISel.h"
 #include "llvm/CodeGen/TargetLoweringObjectFileImpl.h"
 #include "llvm/CodeGen/ValueTypes.h"
 #include "llvm/IR/DiagnosticInfo.h"
 #include "llvm/IR/DiagnosticPrinter.h"
-#include "llvm/Support/Debug.h"
-#include "llvm/Support/ErrorHandling.h"
-#include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
 
