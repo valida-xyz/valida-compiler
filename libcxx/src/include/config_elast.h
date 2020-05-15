5e3b874dbce4eed3b1e260d2d8a30cdffd85e771
Dominik Montada <dominik.montada@hightec-rt.com>
2020-05-15 10:48:29 +0200

    [DPWBS-765] feat(libcxx): add ELAST config for _HAS_DINKUM_CLIB.
    The Dinkum C library can handle unknown error numbers.
    Therefore, defining ELAST is not required.

--- libcxx/src/include/config_elast.h
+++ libcxx/src/include/config_elast.h
@@ -33,6 +33,8 @@
 #define _LIBCPP_ELAST ESTALE
 #elif defined(_LIBCPP_MSVCRT_LIKE)
 #define _LIBCPP_ELAST (_sys_nerr - 1)
+#elif defined(_HAS_DINKUM_CLIB)
+// No _LIBCPP_ELAST needed when using Dinkum libc
 #else
 // Warn here so that the person doing the libcxx port has an easier time:
 #warning ELAST for this platform not yet implemented
